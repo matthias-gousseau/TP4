400dffcca40070ade38d2ba08c78fd4463361518
matthias-gousseau <matthias.gousseau@etu.umontpellier.fr>
2019-02-21 15:51:40 +0100

    TP4

--- src/main/java/fr/umontpellier/iut/IBaille.java
+++ src/main/java/fr/umontpellier/iut/IBaille.java
@@ -2,6 +2,20 @@ package fr.umontpellier.iut;
 
 public class IBaille {
     public static void main(String[] args) {
+        Produit P = new Produit(1, "Un produit", 5, 3,2);
+        Compte Compte1 = new Compte("Compte1", 50);
+        OffreEnchere Offre = new OffreEnchere(0, 15);
+
+        System.out.println();
+        System.out.println(P.toStringProduit());
+        System.out.println();
+        System.out.println(Compte1.toStringCompte());
+        System.out.println();
+        System.out.println(Offre.toStringOffre());
+        System.out.println();
+        P.calculerGagnant(Offre);
+        System.out.println(P.toStringProduit());
+
 
         
     }
--- src/main/java/fr/umontpellier/iut/OffreEnchere.java
+++ src/main/java/fr/umontpellier/iut/OffreEnchere.java
@@ -2,15 +2,15 @@ package fr.umontpellier.iut;
 
 public class OffreEnchere {
     private int prixEnCours;
-    private static int prixMax;
-    //private Produit Product;
+    private int prixMax;
+    /* private Produit Product; */
 
     public OffreEnchere(int prixCour, int prixMax) {
         this.prixEnCours=prixCour;
     }
 
     public String toStringOffre() {
-        return ("\n" + "Prix en cours Enchere : " + this.prixEnCours + "Prix Max Enchere" + this.prixMax);
+        return ("\n" + "Prix en cours Enchere : " + this.prixEnCours + "\n" + "Prix Max Enchere : " + this.prixMax);
     }
 
     public int getPrixEnCours() {
